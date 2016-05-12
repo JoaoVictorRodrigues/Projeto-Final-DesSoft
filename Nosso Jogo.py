98307c3736ec117407344f3bea9036913b125646
JoaoVictorRodrigues <joaovrs1@al.insper.edu.br>
2016-05-12 20:35:06 -0300

    Barreiras vindo em sua direção

--- Nosso Jogo.py
+++ Nosso Jogo.py
@@ -1,7 +1,10 @@
 import pygame
+import random
 pygame.init()
 
+
 white=(255,255,255)
+black=(0,0,0)
 gray=(128,128,128)
 blue=(0,0,255)
 
@@ -13,11 +16,13 @@ pygame.display.update()
 gameExit= False
 
 lead_x=200
-lead_y=550
-#lead_x_change= 0
+lead_y=550 
+
+randParedeX= random.randrange(100,300)
+ParedeY= 0
+
 clock = pygame.time.Clock()
-
-
+lead_y_change= 0
 
 while not gameExit:
      for event in pygame.event.get():
@@ -25,20 +30,28 @@ while not gameExit:
             gameExit=True
         
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
+           lead_y_change =+1
+     
+           if event.key == pygame.K_LEFT:
                 if 100<lead_x<=300:
-                    lead_x -=100  
-            if event.key==pygame.K_RIGHT:
+                    lead_x -=100
+                    
+           if event.key==pygame.K_RIGHT:
                 if 100<=lead_x<300:
                     lead_x +=100
-        
-     
+                    #lead_y_change =+10
+            
+     ParedeY += lead_y_change
      gameDisplay.fill(white) 
+     #Rua 
      pygame.draw.rect(gameDisplay,gray,[100,0,300,650])
+     #Personagem     
      pygame.draw.rect(gameDisplay,blue,[lead_x,lead_y,100,100])
-     
+     #Barreiras
+     pygame.draw.rect(gameDisplay,black,[randParedeX,ParedeY,100,20])
      pygame.display.update()
-     
+     clock.tick(20)
      
 pygame.quit()
+
 quit()

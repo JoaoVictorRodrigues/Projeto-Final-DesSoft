b866680a71193ef0add5b5cda351d0f885e52c0f
JoaoVictorRodrigues <joaovrs1@al.insper.edu.br>
2016-05-12 21:49:02 -0300

    ...

--- Nosso Jogo.py
+++ Nosso Jogo.py
@@ -8,6 +8,8 @@ black=(0,0,0)
 gray=(128,128,128)
 blue=(0,0,255)
 
+clock = pygame.time.Clock()
+
 gameDisplay=pygame.display.set_mode((500,650))
 pygame.display.set_caption("Atrasados do Insper")
 
@@ -21,7 +23,7 @@ lead_y=550
 randParedeX= random.randrange(100,300)
 ParedeY= 0
 
-clock = pygame.time.Clock()
+
 lead_y_change= 0
 
 while not gameExit:
@@ -30,25 +32,31 @@ while not gameExit:
             gameExit=True
         
         if event.type == pygame.KEYDOWN:
-           lead_y_change =+1
+           lead_y_change =+10
      
            if event.key == pygame.K_LEFT:
                 if 100<lead_x<=300:
                     lead_x -=100
                     
-           if event.key==pygame.K_RIGHT:
+           elif event.key==pygame.K_RIGHT:
                 if 100<=lead_x<300:
                     lead_x +=100
                     #lead_y_change =+10
+                    
+       # if ParedeY>650:
             
      ParedeY += lead_y_change
      gameDisplay.fill(white) 
+     
+     
      #Rua 
      pygame.draw.rect(gameDisplay,gray,[100,0,300,650])
      #Personagem     
      pygame.draw.rect(gameDisplay,blue,[lead_x,lead_y,100,100])
      #Barreiras
      pygame.draw.rect(gameDisplay,black,[randParedeX,ParedeY,100,20])
+     
+     
      pygame.display.update()
      clock.tick(20)
      

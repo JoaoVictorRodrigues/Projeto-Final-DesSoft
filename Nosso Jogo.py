359f7b2b396632ce73ac35eb738d6965c7fe7720
felipesigiani <felipe.sigiani.ismart@gmail.com>
2016-05-12 21:43:49 -0300

    estou aqui

--- Nosso Jogo.py
+++ Nosso Jogo.py
@@ -6,7 +6,8 @@ black=(128,128,128)
 blue=(0,0,255)
 
 gameDisplay=pygame.display.set_mode((500,650))
-pygame.display.set_caption("Joao")
+pygame.display.set_caption("Atrasados do Insper")
+
 
 pygame.display.update()
 
@@ -15,7 +16,9 @@ gameExit= False
 lead_x=200
 lead_y=550
 #lead_x_change= 0
+
 clock = pygame.time.Clock()
+
 
 
 
@@ -27,16 +30,20 @@ while not gameExit:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 lead_x -=100
-            if event.key==pygame.K_RIGHT:
+            elif event.key==pygame.K_RIGHT:
                 lead_x +=100
-         
-     
+            elif event.key == pygame.K_LEFT:
+                lead_x -=100
+            elif event.key==pygame.K_RIGHT:
+                lead_x +=100
+
      gameDisplay.fill(white) 
      pygame.draw.rect(gameDisplay,black,[100,0,300,650])
      pygame.draw.rect(gameDisplay,blue,[lead_x,lead_y,100,100])
      
      pygame.display.update()
-     
+
      clock.tick(10)
+     
 pygame.quit()
 quit()
